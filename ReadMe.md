5715d619fff5cfbef7f873ac112d11f0ba2ad5a6
AbhishekBachare <abhishekbachare@gmail.com>
2024-02-25 23:07:40 +0530

    1st main Commit: added index.html & app.js with myApp module.

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+### Learning AngularJS Code
---
+++ app.js
@@ -0,0 +1,7 @@
+// create module:
+var myApp = angular.module("myApp", []);
+
+myApp.controller("MyController", function($scope) {
+  $scope.message = "Hello from AngularJS.";
+});
+
